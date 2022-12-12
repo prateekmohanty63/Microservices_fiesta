56a6004860cc170e4315fb011a6d954c0165baa9
Prateek <prateekmohanty63@gmail.com>
2022-12-12 23:10:55 +0530

    added urls to products

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+
+
+# CRUD OPERATIONS
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self,request): # /api/products
+        pass
+
+    
+    def create(self,request):   # /api/products
+        pass
+
+    def retrive(self,request,pk=None):   # /api/products/<str:id>
+        pass  
+
+    def update(self,request,pk=None):
+        pass
+
+    def destroy(self,request,pk=None):
+        pass
+
+
+
+    
+
