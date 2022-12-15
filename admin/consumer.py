4689c9c0e369c9a71c54b3dd2ee17ba01309326c
Prateek <prateekmohanty63@gmail.com>
2022-12-15 19:12:57 +0530

    Producer-Consumer working

--- admin/consumer.py
+++ admin/consumer.py
@@ -12,6 +12,7 @@ channel.queue_declare(queue='admin')
 
 def callback(ch,method,properties,body):
     print('Received in admin')
+    print(body)
 
 channel.basic_consume(queue='admin',on_message_callback=callback)
 print('Started Consuming')
