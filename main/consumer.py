069d5aa0e08076cb500a6c7158a52da266bdd53c
Prateek <prateekmohanty63@gmail.com>
2022-12-15 19:37:16 +0530

    Communication from admin to main working

---
+++ main/consumer.py
@@ -0,0 +1,22 @@
+import pika
+
+
+params=pika.URLParameters('amqps://gxlccvid:aotLrPEaarM1Gk3YCra7CERnZwfTN9na@cow.rmq2.cloudamqp.com/gxlccvid')
+
+connection=pika.BlockingConnection(params)
+
+channel=connection.channel()
+
+channel.queue_declare(queue='main')
+
+
+def callback(ch,method,properties,body):
+    print('Received in main')
+    print(body)
+
+channel.basic_consume(queue='main',on_message_callback=callback)
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
--- main/producer.py
+++ main/producer.py
@@ -8,4 +8,4 @@ connection=pika.BlockingConnection(params)
 channel=connection.channel()
 
 def publish():
-    channel.basic_publish(exchange='',routing_key='main',body='hello admin')
+    channel.basic_publish(exchange='',routing_key='main',body='hello main')
